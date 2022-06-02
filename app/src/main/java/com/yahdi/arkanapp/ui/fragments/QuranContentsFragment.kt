59e8b5ac04bf749843eeb76e1a68ff684ade100a
Yahdi_2702 <yahdi2702@gmail.com>
2022-06-02 13:16:03 +0700

    Added some Icons

--- app/src/main/java/com/yahdi/arkanapp/ui/fragments/QuranContentsFragment.kt
+++ app/src/main/java/com/yahdi/arkanapp/ui/fragments/QuranContentsFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import com.yahdi.arkanapp.R
 
 class QuranContentsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
